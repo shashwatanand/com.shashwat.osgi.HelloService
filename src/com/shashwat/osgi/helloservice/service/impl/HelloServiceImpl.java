b64c2fd901a7c405a3eb99f03f1b078af74b2277
Shashwat Anand <shashwat.anand@gmail.com>
2015-01-10 13:16:51 +0530

    Modified the package structure

--- src/com/shashwat/osgi/helloservice/service/impl/HelloServiceImpl.java
+++ src/com/shashwat/osgi/helloservice/service/impl/HelloServiceImpl.java
@@ -1,4 +1,6 @@
-package com.shashwat.osgi.helloservice.service;
+package com.shashwat.osgi.helloservice.service.impl;
+
+import com.shashwat.osgi.helloservice.service.HelloService;
 
 public class HelloServiceImpl implements HelloService {
 
